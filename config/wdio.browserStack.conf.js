83ec5e2c9113f31a9e9eaaab0187015ad6a6d43a
enes.erdogan <enes.erdogan@modanisa.com>
2021-11-08 14:24:59 +0300

    deneme

--- config/wdio.browserStack.conf.js
+++ config/wdio.browserStack.conf.js
@@ -109,24 +109,6 @@ exports.config = {
     },
     onComplete: function (exitCode, config, capabilities, results) {
         //console.info("onComplete")
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', './Reports/Allure/allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
-
-            generation.on('exit', function (exitCode) {
-                clearTimeout(generationTimeout)
-
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
-
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
     },
     onReload: function (oldSessionId, newSessionId) {
         //console.info("onReload")
